bc16119e92891bb93e0e6b576aab9a956e62785b
Евгений Наволоков <goustg62@gmail.com>
2025-07-19 13:17:46 +0300

    Add CRUD routers for work with tasks

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import View, ListView, CreateView, UpdateView, DeleteView
 
-from catalog.models import Tag
+from catalog.models import Tag, Task
 
 
 class IndexView(View):
@@ -11,7 +11,7 @@ class IndexView(View):
     def get(self, request):
         return render(request, self.template_name)
 
-
+### > TagsViews < ###
 class TagsCreateView(CreateView):
     model = Tag
     template_name = "catalog/tags_date.html"
@@ -39,3 +39,31 @@ class TagsDeleteView(DeleteView):
     template_name = "catalog/tags_date.html"
     context_object_name = "tags"
     success_url = reverse_lazy("catalog:tags")
+
+### > TasksViews < ###
+class TasksCreateView(CreateView):
+    model = Task
+    template_name = "catalog/tasks_date.html"
+    context_object_name = "tasks"
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:tasks")
+
+
+class TasksListView(ListView):
+    model = Task
+    template_name = "catalog/tasks_list.html"
+    context_object_name = "tasks"
+
+
+class TasksUpdateView(UpdateView):
+    model = Task
+    template_name = "catalog/tasks_date.html"
+    context_object_name = "tasks"
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:tasks")
+
+class TasksDeleteView(DeleteView):
+    model = Task
+    template_name = "catalog/tasks_delete.html"
+    context_object_name = "tasks"
+    success_url = reverse_lazy("catalog:tasks")

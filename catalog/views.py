39ad070fd5ae4603577a391fc89221a20ae4d419
Евгений Наволоков <goustg62@gmail.com>
2025-07-19 12:41:06 +0300

    Add CRUD routers for work with TagsModel

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views.generic import View, ListView, CreateView, UpdateView, DeleteView
+
+from catalog.models import Tag
 
 
 class IndexView(View):
@@ -7,3 +10,32 @@ class IndexView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class TagsCreateView(CreateView):
+    model = Tag
+    template_name = "catalog/tags_date.html"
+    context_object_name = "tags"
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:tags")
+
+
+class TagsListView(ListView):
+    model = Tag
+    template_name = "catalog/tags_list.html"
+    context_object_name = "tags"
+
+
+class TagsUpdateView(UpdateView):
+    model = Tag
+    template_name = "catalog/tags_date.html"
+    context_object_name = "tags"
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:tags")
+
+
+class TagsDeleteView(DeleteView):
+    model = Tag
+    template_name = "catalog/tags_date.html"
+    context_object_name = "tags"
+    success_url = reverse_lazy("catalog:tags")

688bb05d066902f2461c72cad4ec24d2b6a1a34b
Евгений Наволоков <goustg62@gmail.com>
2025-07-19 12:07:16 +0300

    Add path to index page by IndexView router

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import IndexView
+
+app_name = "catalog"
+
+urlpatterns = [
+    path("", IndexView.as_view(), name="index"),
+]

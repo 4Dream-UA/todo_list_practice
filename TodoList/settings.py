7fca9e79fcd5b5f0b5a498ed3acbe56c125f55f7
Евгений Наволоков <goustg62@gmail.com>
2025-07-19 13:45:48 +0300

    Change secret_key to os.getenv from .env file

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -10,17 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
 from pathlib import Path
+
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yn2s8d(%4o-be7y-w8$%@+vv2p-h)dnh1((xwhkd&eirjqq+b('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
